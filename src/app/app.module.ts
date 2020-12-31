ded872503889f60a79864b19c7a055b7a40b92e0
unknown <ashikaga.yoshiaki.xx@gmail.com>
2020-12-31 14:43:30 -0500

    this is first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { MaterialComponent } from './material/material.component';
+
+const routes:Routes = [
+  { path: 'hello', component: HelloComponent},
+  { path: 'material', component: MaterialComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    MaterialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
